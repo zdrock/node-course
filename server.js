91e953fd80fb690ccf8eccb0dd0f97d98960bb70
Vitaliy <vitaliy.zdrok@gmail.com>
2018-07-01 02:15:33 +0300

    Setup start script and herocu port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials')
@@ -24,9 +26,9 @@ app.use((req, res, next)=>{
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname +'/public'));
 
@@ -58,7 +60,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-
-
-app.listen(3000);
+app.listen(port, () => {
+    console.log('Server is up on port 3000');
+});
 
